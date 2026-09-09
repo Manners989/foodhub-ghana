2a338d54200d10b88cedff751074055376eee0e9
Maxwell Nketsiah <nanayawmanners@gmail.com>
2026-09-09 12:36:16 +0000

    Configure media file serving in root urls

--- foodhub/urls.py
+++ foodhub/urls.py
@@ -1,13 +1,12 @@
 from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import include, path, re_path
-from django.views.static import serve
+from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('store.urls')),
     path('accounts/', include('accounts.urls')),
-    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    path('', include('store.urls')),
 ]
 
 if settings.DEBUG:
