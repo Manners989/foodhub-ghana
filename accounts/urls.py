8434b3dac218745cd1d9ac5740f767459699a574
Maxwell Nketsiah <nanayawmanners@gmail.com>
2026-09-09 12:30:07 +0000

    Configure media file serving in root urls

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,12 +1,11 @@
 from django.urls import path
-
 from . import views
 
 app_name = 'accounts'
 
 urlpatterns = [
-    path('register/', views.register, name='register'),
-    path('login/', views.FoodHubLoginView.as_view(), name='login'),
-    path('logout/', views.FoodHubLogoutView.as_view(), name='logout'),
+    path('register/', views.register_view, name='register'),
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
     path('profile/', views.profile, name='profile'),
 ]
